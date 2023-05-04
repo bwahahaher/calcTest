b54a1cfe70e6d3de72ba937f5e2875d54c0659ca
влад <batsyhc@gmail.com>
2023-05-04 09:25:15 +0500

    Initial Commit

---
+++ calcTest/calcTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcTestApp.swift
+//  calcTest
+//
+//  Created by UNIFORM on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct calcTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
